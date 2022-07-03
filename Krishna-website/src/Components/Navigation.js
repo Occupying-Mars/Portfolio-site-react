4c7c6be774acc75a13e37a93c56fdc74d37d15d1
Occupying-Mars <72188133+Occupying-Mars@users.noreply.github.com>
2022-07-03 15:35:09 +0530

    Update Navigation.js

--- Krishna-website/src/Components/Navigation.js
+++ Krishna-website/src/Components/Navigation.js
@@ -20,9 +20,6 @@ function Navigation() {
                     <NavLink to="/resume" activeClassName="active-class" exact>Resume</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to="/portfolios" activeClassName="active-class" exact>Portfolios</NavLink>
-                </li>
-                <li className="nav-item">
                     <NavLink to="/blogs" activeClassName="active-class" exact>Blogs</NavLink>
                 </li>
                 <li className="nav-item">
